640d7937060ad5c8c46f96d5f9721c5173d9592b
Sabastian <sswbelser@gmail.com>
2015-07-23 12:20:55 -0700

    Tests work

--- test/test.js
+++ test/test.js
@@ -18,11 +18,92 @@ describe("Game Title", function() {
 	});
 });
 
-describe('DELETE /api/comments/:id', function() {
-  it('should return statusCode 200', function(done) {
-    request.del(baseUrl + '/api/comments/' + commentId, function(error, response, body) {
-      expect(response.statusCode).to.equal(200);
-      done();
-    });
-  });
+describe('GET /', function() {
+	it('should return statusCode 200', function(done) {
+		request(baseUrl, function(error, response, body) {
+			expect(response.statusCode).to.equal(200);
+			done();
+		});
+	});
+});
+
+describe('GET /api/comments', function() {
+	it('should return statusCode 200', function(done) {
+		request(baseUrl + '/api/comments', function(error, response, body) {
+			expect(response.statusCode).to.equal(200);
+			done();
+		});
+	});
+});
+
+describe('POST /api/comments', function() {
+	it('should return statusCode 200', function(done) {
+		request.post(
+			{
+				url: baseUrl + '/api/comments',
+				form: {
+					username: 'Sabo',
+					comment: 'I love it!'
+				}
+			},
+			function(error, response, body) {
+				expect(response.statusCode).to.equal(200);
+				done();
+			}
+		);
+	});
+});
+
+describe('GET /api/users', function() {
+	it('should return statusCode 200', function(done) {
+		request(baseUrl + '/api/comments', function(error, response, body) {
+			expect(response.statusCode).to.equal(200);
+			done();
+		});
+	});
+});
+
+describe('POST /api/users', function() {
+	it('should return statusCode 200', function(done) {
+		request.post(
+			{
+				url: baseUrl + '/api/comments',
+				form: {
+					username: 'TestUser',
+					password: 'thisisatest'
+				}
+			},
+			function(error, response, body) {
+				expect(response.statusCode).to.equal(200);
+				done();
+			}
+		);
+	});
+});
+
+describe('GET /login', function() {
+	it('should return statusCode 200', function(done) {
+		request(baseUrl + '/api/comments', function(error, response, body) {
+			expect(response.statusCode).to.equal(200);
+			done();
+		});
+	});
+});
+
+describe('POST /login', function() {
+	it('should return statusCode 200', function(done) {
+		request.post(
+			{
+				url: baseUrl + '/api/comments',
+				form: {
+					username: 'TestUser',
+					password: 'thisisatest'
+				}
+			},
+			function(error, response, body) {
+				expect(response.statusCode).to.equal(200);
+				done();
+			}
+		);
+	});
 });
